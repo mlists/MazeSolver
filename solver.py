6b3aff694bba74e65059bcc2386d9eebc9fd659a
Michael Kennedy <michael.kennedy@thedropbears.org.au>
2020-03-13 10:41:41 +1100

    Working cartesian iteration

--- solver.py
+++ solver.py
@@ -8,14 +8,25 @@ class Maze():
     """
     def __init__(self, array_2d: List[List[int]]):
         self.rows = array_2d
-        # These are in machine coordinates
-        self.current_y = len(self.rows)
-        self.current_x = 0
-        self.current_item = None
 
         # Defines the iteration orders, defaults to conventional cartesian
         self.x_right: bool = True
         self.y_up: bool = True
+
+        # in machine coordinates
+        self.current_x: int = None
+        self.current_y: int = None
+
+        self._init_x()
+        self._init_y()
+    
+    def _init_x(self):
+        # initilise so first next returns the correct first item from the array
+        self.current_x = 0 if self.x_right else len(self.rows[0]) - 1
+
+    def _init_y(self):
+        # initilise so first next returns the correct first item from the array
+        self.current_y = len(self.rows) - 1 if self.y_up else 0
 
     def __iter__(self):
         return self
@@ -24,7 +35,25 @@ class Maze():
         """
         Iterates through the 2d array in the currently specified order
         """
-        pass
+        x, y = self.current_x, self.current_y
+        self.current_x += 1 if self.x_right else -1
+
+        if not (-1 < x < len(self.rows)):
+            # Finished row, move to next one
+            self._init_x()
+            self.current_y -= 1 if self.y_up else -1
+            x, y = self.current_x, self.current_y
+            self.current_x += 1 if self.x_right else -1
+
+            if not (-1 < y < len(self.rows) - 1):
+                # Finished cycling, escape calling loop
+                # reinitialise for next cycle
+                self._init_x()
+                self._init_y()
+                raise StopIteration
+        
+        return self.rows[y][x]
+
 
     def __getitem__(self, x: int, y: int) -> int:
         """
@@ -51,13 +80,14 @@ class Maze():
         """
         Setup the iteration order of the class, this sticks untill reset. With both
         booleans True the array will loop right and upon completion of a row will move
-        up.
+        up. Calling this will reset iteration if it is currently running.
 
         @param
         x_right: bool - If True, the x-axis iterates right
         y_up: bool - If True, the y-axis iterates upwards
         """
-        pass
+        self.x_right = x_right
+        self.y_up = y_up
 
 class Solver():
     def __init__(self):
@@ -65,3 +95,15 @@ class Solver():
 
     def take_input(self, maze):
         self.maze = maze
+
+test_grid = [
+[12, 13, 14, 15],
+[8, 9, 10, 11],
+[4, 5, 6, 7],
+[0, 1, 2, 3],
+]
+
+maze = Maze(test_grid)
+
+for num in maze:
+    print(num)
