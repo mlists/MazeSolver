d4d87111e32a32d9afacb2228b4f67bacf265817
Michael Kennedy <michael.kennedy@thedropbears.org.au>
2020-03-13 11:25:03 +1100

    Indexing working

--- solver.py
+++ solver.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 class Maze():
     """
@@ -17,16 +17,19 @@ class Maze():
         self.current_x: int = None
         self.current_y: int = None
 
+        self.y_max = len(self.rows) - 1
+        self.x_max = len(self.rows[0]) - 1
+
         self._init_x()
         self._init_y()
     
     def _init_x(self):
         # initilise so first next returns the correct first item from the array
-        self.current_x = 0 if self.x_right else len(self.rows[0]) - 1
+        self.current_x = 0 if self.x_right else self.x_max
 
     def _init_y(self):
         # initilise so first next returns the correct first item from the array
-        self.current_y = len(self.rows) - 1 if self.y_up else 0
+        self.current_y = self.y_max if self.y_up else 0
 
     def __iter__(self):
         return self
@@ -38,14 +41,14 @@ class Maze():
         x, y = self.current_x, self.current_y
         self.current_x += 1 if self.x_right else -1
 
-        if not (-1 < x < len(self.rows)):
+        if not (-1 < x < self.x_max + 1):
             # Finished row, move to next one
             self._init_x()
             self.current_y -= 1 if self.y_up else -1
             x, y = self.current_x, self.current_y
             self.current_x += 1 if self.x_right else -1
 
-            if not (-1 < y < len(self.rows)):
+            if not (-1 < y < self.y_max + 1):
                 # Finished cycling, escape calling loop
                 # reinitialise for next cycle
                 self._init_x()
@@ -55,18 +58,17 @@ class Maze():
         return self.rows[y][x]
 
 
-    def __getitem__(self, x: int, y: int) -> int:
+    def __getitem__(self, indices: Tuple[int, int]) -> int:
         """
-        Returns the value at the specified x, y coordinate, this will be in conventional
-        cartesian form. i.e. with the x coordinate first, right positive and the y
-        coordinate second being up positive. The first item in each dimension is at
-        index 0, this object supports negative indicies with the direction of the index
+        Returns the value at the specified x, y coordinate, uses the current dimesion set.
+        This object supports negative indicies with the direction of the index
         being reversed. This method can also be accessed with normal index notation
         thanks to python magic methods.
 
         @param
-        x: int - The x coordinate to fetch
-        y: int - The y coordinate to fetch
+        indicies: Tuple[x: int, y: int]
+            x: int - The x coordinate to fetch
+            y: int - The y coordinate to fetch
 
         @returns
         The value at the specified coordinates
@@ -74,13 +76,21 @@ class Maze():
         @raises
         Will raise an index error if the coordinate is not avaliable
         """
-        return
+        x, y = indices
+        # Reverse axis as required
+        x = x if self.x_right else self.x_max - x
+        y = self.y_max - y if self.y_up else y
+        if abs(x) <= self.x_max:
+            if abs(y) <= self.y_max:
+                return self.rows[y][x]
+        raise IndexError
     
-    def configure_iteration(self, x_right: bool, y_up: bool) -> None:
+    def configure_dimensions(self, x_right: bool, y_up: bool) -> None:
         """
         Setup the iteration order of the class, this sticks untill reset. With both
         booleans True the array will loop right and upon completion of a row will move
-        up. Calling this will reset iteration if it is currently running.
+        up. Calling this will reset iteration if it is currently running. This also
+        affects the origin of the object for the purposes of indexing.
 
         @param
         x_right: bool - If True, the x-axis iterates right
@@ -106,7 +116,8 @@ test_grid = [
 ]
 
 maze = Maze(test_grid)
-maze.configure_iteration(x_right=False, y_up=False)
+maze.configure_dimensions(x_right=True, y_up=False)
 
-for num in maze:
-    print(num)
+# for num in maze:
+#     print(num)
+print(maze[0, 0])
